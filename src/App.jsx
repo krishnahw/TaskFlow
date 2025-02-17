aaa2f2e33804f21e8ac7ce56b4b8be97bebade8f
krishnahw <krishnavishwakarma7823@gmail.com>
2025-02-17 09:32:39 +0530

    updated App.jsx with latest change

--- src/App.jsx
+++ src/App.jsx
@@ -6,62 +6,58 @@ import EmployeeDashboard from "./component/Dashboard/EmployeeDashboard";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
-
-
   const [user, setUser] = useState(null);
   const [logInUser, setLogInUser] = useState(null);
-  const [authData , setUserDta] = useContext(AuthContext);
+  const [authData, setUserDta] = useContext(AuthContext);
 
   console.log(authData);
-  
 
   useEffect(() => {
     const storeUser = JSON.parse(localStorage.getItem("login"));
 
-  console.log(storeUser);
-  
-  
-  
+    console.log(storeUser);
+
     if (storeUser) {
       setUser(storeUser.role);
-      setLogInUser(storeUser.data)
-     
+      setLogInUser(storeUser.data);
     }
-  }, [])
-  
+  }, []);
 
   const handlelogin = (email, password) => {
- 
-  
     if (email === "admin@me.com" && password === "123") {
       setUser("admin");
       setLogInUser(authData.admin);
-      localStorage.setItem("login", JSON.stringify({ role: "admin" , data:authData.admin }));
-       
-  
-     
+      localStorage.setItem(
+        "login",
+        JSON.stringify({ role: "admin", data: authData.admin })
+      );
     } else if (authData) {
-      const employee = authData.employees.find((e) => email === e.email && password === e.password);
+      const employee = authData.find(
+        (e) => email === e.email && password === e.password
+      );
       if (employee) {
         setUser("employee");
         setLogInUser(employee);
-        localStorage.setItem("login", JSON.stringify({ role: "employee", data:employee }));
-        
-  
-  
+        localStorage.setItem(
+          "login",
+          JSON.stringify({ role: "employee", data: employee })
+        );
       }
     } else {
       alert("Invalid email/password");
     }
-  }
-  
+  };
 
   return (
     <>
       {!user && <Login handlelogin={handlelogin} />}
 
-      {user === "admin" && <AdminDashboard setUser={setUser} data={logInUser}/>}
-      {user === "employee" && <EmployeeDashboard setUser={setUser} data={logInUser} />}
+      {user === "admin" && (
+        <AdminDashboard setUser={setUser} data={logInUser} />
+      )}
+      {user === "employee" && (
+        <EmployeeDashboard setUser={setUser} data={logInUser} />
+      )}
     </>
   );
 }
